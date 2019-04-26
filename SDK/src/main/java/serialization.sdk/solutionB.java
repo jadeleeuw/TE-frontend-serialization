5282071c652ffcb962c701246973b4bd8e69eea8
Jean de Leeuw <jeandeleeuw@gmail.com>
2019-04-26 10:57:15 +0200

    updated assignments

--- SDK/src/main/java/serialization.sdk/solutionB.java
+++ SDK/src/main/java/serialization.sdk/solutionB.java
@@ -1,8 +1,21 @@
 package serialization.sdk;
 
+import sdk.serialization.ApiClient;
+import sdk.serialization.api.AssignmentBApi;
 import sdk.serialization.model.SubmissionResult;
 
 public class solutionB implements assignmentB {
+
+    private AssignmentBApi api;
+
+    // Enter your identification key here
+    private final int ID_KEY = ;
+
+    public solutionB() {
+        ApiClient client =  new ApiClient();
+        client.setApiKey(String.valueOf(ID_KEY));
+        this.api = new AssignmentBApi(client);
+    }
 
     @Override
     public SubmissionResult solutionB() {
