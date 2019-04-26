ce427ac1f588479aed60008be46ab7917855a0d4
Jean de Leeuw <jeandeleeuw@gmail.com>
2019-04-26 10:54:34 +0200

    Updated SDK endpoints

--- SDK/src/main/java/serialization.sdk/solutionA.java
+++ SDK/src/main/java/serialization.sdk/solutionA.java
@@ -1,8 +1,21 @@
 package serialization.sdk;
 
+import sdk.serialization.ApiClient;
+import sdk.serialization.api.AssignmentAApi;
 import sdk.serialization.model.SubmissionResult;
 
 public class solutionA implements assignmentA {
+
+    private AssignmentAApi api;
+
+    // Enter your identification key here
+    private final int ID_KEY = ;
+
+    public solutionA() {
+        ApiClient client =  new ApiClient();
+        client.setApiKey(String.valueOf(ID_KEY));
+        this.api = new AssignmentAApi(client);
+    }
 
     // Implement assignment A1 here
     @Override
